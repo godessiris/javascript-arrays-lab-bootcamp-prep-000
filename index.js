cfde9c0dc2edb5bc7ccea2865c70a2cce49ebb5e
Iris Quintero <godessiris@gmail.com>
2019-04-23 04:58:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,18 +29,6 @@ function prependKitten(name){
   return [name, ...kittens];
 }
 
-
-
-
-
-function removeFirstKitten() {
-  return kittens.slice(1)
-}
-
-function removeFirstKitten(){
-  return kittens.slice(1);
-}
-
 function removeLastKitten(){
   return kittens.slice(0,kittens.length -1);
 }

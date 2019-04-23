8d92f3c3aa0033668afb80cf17228109d58a5b1d
Iris Quintero <godessiris@gmail.com>
2019-04-23 04:57:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,12 +31,36 @@ function prependKitten(name){
 
 
 
-function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
-}
+
 
 function removeFirstKitten() {
   return kittens.slice(1)
 }
 
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
 
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length -1);
+}
+
+
+describe('removeFirstKitten()', function() {
+    it('removes the first kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      window.kittens = ['Milo', 'Otis', 'Garfield'];
+      expect(removeLastKitten()).toEqual(["Otis", "Garfield"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
+
+  describe('removeLastKitten()', function() {
+    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
+      window.kittens = ['Milo', 'Otis', 'Garfield'];
+      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
+
+      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
+    })
+  })
+})
